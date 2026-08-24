4b9e82e5a62f99a90e23210c12fab67913f4ae98
Imanisolace <148961934+Imanisolace@users.noreply.github.com>
2026-08-24 14:14:25 +0300

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -5,7 +5,7 @@ from sieve import GeometricSieve
 sieve = GeometricSieve()
 
 # 100,000 random true 3D triangles
-pos = np.random.randn(100_000, 3, 3)
+pos = np.random.randn(100000, 3, 3)
 
 t0 = time.time()
 for p in pos:
